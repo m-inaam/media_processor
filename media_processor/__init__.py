ad446e4a854bfef67a4300bfb6a6a1bfedcba5e4
Muhammad Inaamullah <94857175+m-inaam@users.noreply.github.com>
2023-10-22 01:30:54 +0500

    Reading Images & Videos

---
+++ media_processor/__init__.py
@@ -0,0 +1,13 @@
+from media_processor import video_processor, image_processor
+
+if __name__ == "__main__":
+    main_directory = 'C:/Users/muham/OneDrive/Desktop/computer-vision/Resources'
+
+    video_processor_instance = video_processor.VideoProcessor(main_directory)
+    video_processor_instance.process_videos()
+
+    photo_folder_name = 'Photos'
+    image_extensions = ('.png', '.jpg', '.jpeg', '.gif', '.bmp')
+
+    image_processor = ImageProcessor(main_directory, photo_folder_name, image_extensions)
+    image_processor.process_images()
